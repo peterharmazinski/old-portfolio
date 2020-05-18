e573d08bdcf638cdd6e1397e71c34bf4f558e355
Peter Harmazinski <pharmazinski@gmail.com>
2020-05-17 20:10:00 -0500

    add build files for deployment on pythonanywhere

---
+++ react/build/precache-manifest.0794679d1d06fc32167a46c5ce5e3228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472ed467a9e0daf0bf46eb9351607f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d9d7fa8cc6a1b9c763b",
+    "url": "/static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9d7fa8cc6a1b9c763b",
+    "url": "/static/js/main.89c95500.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
